a213bc2f7f0839fc2c93feb9b5afcac83c8629fb
dukesook <devonsookhoo14@gmailcom>
2025-03-01 10:41:24 -0700

    new file: package.json after running $ npm init

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// Node.js is an asynchronous event-driven JavaScript runtime
+// Node.js uses the V8 JavaScript engine from Google Chrome
+
 console.log('Hello World!');
+
+// global is a global object in Node.js
+global.my_num = 5;
+console.log('global.my_num', global.my_num);
+
+console.log('process.platform', process.platform);
+
+
+process.on('exit', function() {
+    console.log('Exiting...');
+})
