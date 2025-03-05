0c8075c3dc69311671119075d1d6cf46753788eb
dukesook <devonsookhoo14@gmailcom>
2025-03-04 22:00:16 -0700

    connect a user to the web socketgit diff!

--- public/client.mjs
+++ public/client.mjs
@@ -1,1 +1,10 @@
 console.log('client.mjs loaded');
+
+
+// MAGIC!!! **************************************** */
+// io was imported by the script tag in index.html
+// When you run a Socket.io server with Express, it automatically
+// serves the client-side script at /socket.io/socket.io.js
+//************************************************* */
+const socket = io(); // Connect to WebSocket server
+//************************************************* */
